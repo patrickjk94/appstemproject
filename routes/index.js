ccc9f7b2c98abba7ce2a521e89a3b0efe23d61e5
patrickjk94 <patrickkennedy@Patricks-MacBook-Pro.local>
2023-10-07 12:53:35 -0700

    feat: installed axios to fetch html data

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { v4: uuidv4 } = require('uuid');
 const ArticleModel =  require("../models/article.js");
+const axios = require('axios');
 
 const API_KEY = "apikey"; 
 
@@ -25,33 +26,51 @@ router.get('/', (req, res) => {
 });
 
 
-
 /** 
  * 
  */
+const fetchAndStoreHTML = async (url, res) => {
+  try {
+    // Fetch HTML of the webpage
+    const response = await axios.get(url);
+    const html = response.data;
+
+    // Create and save the article
+    const article = new ArticleModel({
+      url: url,
+      content: html,
+      job_id: uuidv4().toString(),
+      date: new Date(),
+    });
+
+    await article.save();
+
+    // Send the response
+    res.status(200).send(article.job_id);
+
+  } catch (error) {
+    if (error.response) {
+      console.error('Error fetching the webpage:', error);
+      res.status(500).send('Error fetching the webpage');
+    } else {
+      console.error('Error:', error);
+      res.status(500).send('An unexpected error occurred');
+    }
+  }
+};
+
+
 
 router.get('/save-url/:url', checkApiKey, async (req, res) => {
   /* #swagger.security = [{ "apiKeyAuth": [] }] */
   /* #swagger.parameters['url'] = { description: 'URL to download' } */
 
-  const url = req.params.url;
+  const url = req.params.url; // TODO: sanitize and validate 
   if (!url) {
       return res.status(400).json({ error: 'URL is required' });
   }
 
-  const article = new ArticleModel({
-    url: url,
-    content: '<html> TEST HTML </html>',
-    job_id: uuidv4().toString(),
-    date: new Date(),
-  });
-
-  try {
-    await article.save();
-    res.status(200).send(article.job_id);
-  } catch (error) {
-    res.status(500).send(error);
-  }
+  fetchAndStoreHTML(url, res);
 
 });
 
